8f262319b542cf881fc3bd1ca29cba2d8c67270c
Benjamin C <benjamin@coriou.net>
2019-04-02 09:23:26 +0200

    Monkey spinner
    Best feature yet

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,23 +1,29 @@
 import React from "react"
 import { Box, Color } from "ink"
+import Spinner from "ink-spinner"
 
 import { useStore } from "../context"
 
 export default () => {
 	const [{ isComputing, isCheckingGames, computingGames }] = useStore()
 
+	const ComputingStatus = ({ computingGames }) => (
+		<>
+			<Spinner type="monkey" />{" "}
+			{`Working on ${computingGames.length} game(s) [${computingGames
+				.map(g => g.opponent)
+				.join(",")}]`}
+		</>
+	)
+
 	return (
 		<Box textWrap="truncate-end">
 			<Color bold={isComputing || isCheckingGames} blue>
 				{isCheckingGames ? "Checking chess.com" : null}
 				{isComputing && !computingGames.length ? "Working..." : null}
-				{isComputing && computingGames.length
-					? `Working on ${
-							computingGames.length
-					  } game(s) [${computingGames
-							.map(g => g.opponent)
-							.join(",")}]`
-					: null}
+				{isComputing && computingGames.length ? (
+					<ComputingStatus computingGames={computingGames} />
+				) : null}
 				{!isCheckingGames && !isComputing ? "Waiting..." : null}
 			</Color>
 		</Box>
